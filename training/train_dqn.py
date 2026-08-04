4b7c11fb09732d6dc3295307b725a746516c3498
ajay1951 <ajaygollapudi1951@gmail.com>
2026-08-04 16:04:08 +0530

    fix: QuantAgent parameters in train_dqn.py

--- training/train_dqn.py
+++ training/train_dqn.py
@@ -77,7 +77,7 @@ def train_universal_dqn(epochs_per_window=5):
     # The brain now accepts 18 features!
     meta_agent = MetaAgent(input_dim=18, buffer_size=100000, batch_size=64)
     risk_agent = RiskAgent(0.02, 0.20)
-    quant_agent = QuantAgent(z_score_threshold=2.0)
+    quant_agent = QuantAgent(2.0)
     sentiment_agent = SentimentAgent()
     
     epsilon = 1.0
